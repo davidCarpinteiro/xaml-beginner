16a34c056689454ca79bc9dbee1b49875a1241e9
David Carpinteiro <david_carpinteiro@hotmail.com>
2015-10-08 18:08:36 +0100

    updated main page in app

--- RestaurantManager/RestaurantManager.UniversalWindows/App.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/App.xaml.cs
@@ -65,7 +65,7 @@ namespace RestaurantManager.UniversalWindows
             {
                 // When the navigation stack isn't restored navigate to the first page, configuring
                 // the new page by passing required information as a navigation parameter
-                rootFrame.Navigate(typeof(OrderPage), e.Arguments);
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
             }
             // Ensure the current window is active 
             Window.Current.Activate();
