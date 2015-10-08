2bd125a6ab83285484852c31cceaddee8751d59a
David Carpinteiro <david_carpinteiro@hotmail.com>
2015-10-08 18:55:10 +0100

    Homework 2

--- RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
@@ -1,30 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238 
 
 namespace RestaurantManager.UniversalWindows
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame. 
     /// </summary>
     public sealed partial class ExpeditePage : Page
     {
         public ExpeditePage()
         {
             this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnGoHome control. 
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event. 
+        /// </param>
+        /// <param name="e">
+        /// The <see cref="RoutedEventArgs" /> instance containing the event data. 
+        /// </param>
+        private void btnGoHome_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
--- RestaurantManager/RestaurantManager.UniversalWindows/MainPage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/MainPage.xaml.cs
@@ -1,30 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 
 
 namespace RestaurantManager.UniversalWindows
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame. 
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnGoToOrders control. 
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event. 
+        /// </param>
+        /// <param name="e">
+        /// The <see cref="RoutedEventArgs" /> instance containing the event data. 
+        /// </param>
+        private void btnGoToOrders_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(OrderPage));
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnSubmitOrder control. 
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event. 
+        /// </param>
+        /// <param name="e">
+        /// The <see cref="RoutedEventArgs" /> instance containing the event data. 
+        /// </param>
+        private void btnSubmitOrder_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ExpeditePage));
         }
     }
 }
